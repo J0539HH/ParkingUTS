2ed3315a2821dc20b6c335b116354b2a328542cf
J0539HH <master.joseph@hotmail.com>
2023-03-31 01:58:18 -0500

    Empresariales Final

--- modulos/tareasmenu/Seguimiento.js
+++ modulos/tareasmenu/Seguimiento.js
@@ -3,90 +3,34 @@ var idUsuario = null;
 
 $(document).ready(function () {
   verificarSesion();
-  LimpiarFormulario();
+  LimpiarDatos();
 
   $("#VolverMenu").on("click", function () {
     window.location.href = "../tareasmenu/menu.html";
   });
 
-  $("#btnSolicitar").on("click", function () {
-    ValidarFormulario();
+  $("#btnConsultar").on("click", function () {
+    ConsultarServicio();
+  });
+
+  const inputNumeroServicio = $("#numeroServicio");
+
+  inputNumeroServicio.on("input", function () {
+    const inputValue = inputNumeroServicio.val();
+    if (isNaN(inputValue)) {
+      // Eliminar el último carácter si no es un número
+      inputNumeroServicio.val(inputValue.substring(0, inputValue.length - 1));
+    }
   });
 });
 
-function enviarCorreo(datos) {
-  const mensaje = `<p>Hola</p> <b>${datos.usuario.nombre}</b><p>Acabamos de recibir exitosamente el ingreso del servicio con identificador:</p>
-  <p style="color:#1664ab; font-size: 20px">${datos.servicio.idservicio}</p>
-  <p>Puedes realizar el seguimiento en el portal con el número anterior</p>
-  <b>Estos son los datos de tu servicio</b>
-  <br><b>Tipo de dispositivo:&nbsp;</b>${datos.servicio.tipodispositivo} 
-  <br><b>Marca:&nbsp;</b>${datos.servicio.marca}
-  <br><b>Informacion sobre el mantenimiento:&nbsp;</b>${datos.servicio.comentariosentrada}
-  <br><br>Gracias por confiarnos la solicitud de tu mantenimiento!`;
-  const correo = datos.usuario.correo;
-  const asunto = "Ingreso de servicio (Mantenimiento)";
+function ConsultarServicio() {
+  let idservicio = parseInt($("#numeroServicio").val());
 
+  spinner("Cargando datos del servicio, por favor espere");
+  const url = "/api/servicioEspecifico";
   const data = {
-    correo: correo,
-    asunto: asunto,
-    mensaje: mensaje,
-  };
-
-  fetch("/EnvioDecorreo", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (response.ok) {
-        console.log("Correo electrónico enviado correctamente");
-      } else {
-        throw new Error("Error al enviar el correo electrónico");
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-function ValidarFormulario() {
-  let ComentariosEntrada = $("#comentariosEntrada").val();
-  let Marca = $("#marca").val();
-  let TipoDispositivo = $("#tipoDispositivo").val();
-
-  if (ComentariosEntrada === "") {
-    AlertIncorrecta(
-      "Debes indicar alguna informacion sobre el mantenimiento del dispositivo"
-    );
-    return;
-  }
-  if (Marca === "") {
-    AlertIncorrecta("Debes indicar la marca del dispositivo");
-    return;
-  }
-  if (TipoDispositivo === "") {
-    AlertIncorrecta("Debes seleccionar un tipo de dispositivo");
-    return;
-  }
-  RealizarInsercion();
-}
-
-function RealizarInsercion() {
-  spinner("Registrando servicio, por favor espere");
-  let comentariosentrada = $("#comentariosEntrada").val();
-  let marca = $("#marca").val();
-  let tipodispositivo = $("#tipoDispositivo").val();
-  let numeroserie = $("#numeroSerie").val();
-
-  const url = "/api/NewForm";
-  const data = {
-    idusuario: idUsuario,
-    comentariosentrada,
-    marca,
-    tipodispositivo,
-    numeroserie,
+    idservicio: idservicio,
   };
   fetch(url, {
     method: "POST",
@@ -97,20 +41,39 @@ function RealizarInsercion() {
   })
     .then((response) => response.json())
     .then((result) => {
-      enviarCorreo(result);
-      AlertCorrectX("Servicio registrado en el sistema ");
+      console.log(result);
+      cargarDatos(result);
       $("#spinner").hide();
-      LimpiarFormulario();
     })
     .catch((error) => {
-      console.error("Error al registrar:", error);
+      console.log(error);
+      AlertIncorrecta("No se pudo cargar el servicio");
+      LimpiarDatos();
       $("#spinner").hide();
     });
 }
+function cargarDatos(data) {
+  $("#detallesServicio").removeClass("hidden");
 
-function LimpiarFormulario() {
-  $("#comentariosEntrada").val("");
-  $("#marca").val("");
-  $("#tipoDispositivo").val("");
-  $("#numeroSerie").val("");
+  $("#comentariosCliente").html(data.comentariosentrada);
+  $("#marca").html(data.marca);
+  $("#tipoDispositivo").html(data.tipodispositivo);
+  $("#estado").html(data.estado);
+  $("#numeroSerie").html(data.numeroserie);
+  $("#comentariosTecnico").html(data.comentariossalida);
+  $("#memoriaRam").html(data.ram);
+  $("#tipoAlmacenamiento").html(data.tipodisco);
+}
+
+function LimpiarDatos() {
+  $("#numeroServicio").val("");
+  $("#detallesServicio").addClass("hidden");
+  $("#comentariosCliente").empty();
+  $("#marca").empty();
+  $("#tipoDispositivo").empty();
+  $("#memoriaRam").empty();
+  $("#tipoAlmacenamiento").empty();
+  $("#numeroSerie").empty();
+  $("#estado").empty();
+  $("#comentariosTecnico").empty();
 }
